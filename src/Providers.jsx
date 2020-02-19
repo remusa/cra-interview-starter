1346bc591d5649e69035f27c3e27e189d42f0639
R Sánchez <rene.as.himself@gmail.com>
2020-02-18 22:22:03 -0600

    Moved Providers to index

--- src/Providers.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import { BrowserRouter as Router } from 'react-router-dom'
-
-const Providers = ({ children }) => {
-  return <Router>{children}</Router>
-}
-
-export default Providers
--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { BrowserRouter as Router } from 'react-router-dom'
 import App from './App'
 import './index.css'
-import Providers from './Providers'
+
+const Providers: React.FC<{ children: React.ReactElement }> = ({
+  children,
+}) => {
+  return <Router>{children}</Router>
+}
 
 ReactDOM.render(
   <Providers>
